b49d03affe080b16264cc47f0ac9928ea52b82f4
Teté <tete@tete-Lenovo-B575e.(none)>
2014-02-09 22:55:00 +0100

    Luz y distancia

---
+++ robotsensors/robotsensors.ino
@@ -0,0 +1,45 @@
+/* Sigue luz sin colisionar*/
+#include <RobotMovil.h>
+RobotMovil robot;
+
+#include <NewPing.h>
+/*NewPing sonar_L(10,10,50);
+NewPing sonar_R(11,11,50);*/
+NewPing sonar(5,A0,50);
+float dist;
+int eye_R = A3;
+int eye_L = A5;
+int light_R;
+int light_L;
+
+void setup()
+{
+Serial.begin(9600);
+}
+
+void loop()
+{
+  dist = sonar.ping_cm();
+  
+  if(dist == 0){dist=50;}
+  if(dist>15)
+  {
+  delay(30);
+  light_R = analogRead(eye_R);
+  Serial.print(light_R);
+  Serial.println(" right");
+
+  delay(30);
+  light_L = analogRead(eye_L); 
+  Serial.print(light_L);
+  
+  Serial.println(" left");
+
+  int dist=min(light_L,light_R);
+ 
+  if (light_L>light_R) {robot.forward(100,25,1);}
+  else if(light_L<light_R) {robot.forward(25,100,1);}
+  else {robot.forward(100,100,1);}
+  }
+  else {robot.rotate(1);}
+}
